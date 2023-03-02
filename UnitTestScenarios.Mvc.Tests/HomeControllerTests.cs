d1fafb783311784c407141e816a4c5c0d26941c9
seymacengiz <seyma@abisteknoloji.com.tr>
2023-03-02 10:44:17 +0100

    Mvc and test projects added to the solution. HomeController methods tested.

---
+++ UnitTestScenarios.Mvc.Tests/HomeControllerTests.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Moq;
+using UnitTestScenarios.Mvc.Controllers;
+
+namespace UnitTestScenarios.Mvc.Tests
+{
+    [TestFixture]
+    public class HomeControllerTests
+    {
+        private HomeController _controller;
+
+        [SetUp]
+        public void Initialize()
+        {
+            var loggerMock = new Mock<ILogger<HomeController>>();
+            _controller = new HomeController(loggerMock.Object);
+        }
+
+        [Test]
+        public void HomeController_Index_ReturnsView()
+        {
+
+            var actResult = _controller.Index() as ViewResult;
+
+            Assert.IsNotNull(actResult);
+            Assert.That(actResult.ViewName, Is.EqualTo("Index"));
+        }
+
+        [Test]
+        public void HomeController_Privacy_ReturnsView()
+        {
+
+            var actResult = _controller.Privacy() as ViewResult;
+
+            Assert.IsNotNull(actResult);
+            Assert.That(actResult.ViewName, Is.EqualTo("Privacy"));
+        }
+    }
+}
---
+++ UnitTestScenarios.Mvc/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using UnitTestScenarios.Mvc.Models;
+
+namespace UnitTestScenarios.Mvc.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View("Index");
+        }
+
+        public IActionResult Privacy()
+        {
+            return View("Privacy");
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View("Error", new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
